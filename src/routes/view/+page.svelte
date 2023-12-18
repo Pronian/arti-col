32f6609067c6a77b2e5807a5e1b6169f62497a74
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2023-12-18 22:01:40 +0200

    Fixed possible undefined article in template

--- src/routes/view/+page.svelte
+++ src/routes/view/+page.svelte
@@ -2,25 +2,31 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	const article = data.article;
 </script>
 
 <svelte:head>
-	<title>{data.article.title || 'View article'} | ArtiCol</title>
+	{#if article}
+		<title>{article.title || 'View article'} | ArtiCol</title>
+	{/if}
 </svelte:head>
 
 <article>
-	{#if data.article.title}
-		<h1>{data.article.title}</h1>
+	{#if article}
+		{#if article.title}
+			<h1>{article.title}</h1>
+		{/if}
+		<div class="article-meta">
+			{#if article.author}
+				<div>By <span>{article.author.replace('@', '')}</span></div>
+			{/if}
+			{#if article.published}
+				<div>Published on <span>{new Date(article.published).toLocaleDateString()}</span></div>
+			{/if}
+		</div>
+		{@html article.content}
 	{/if}
-	<div class="article-meta">
-		{#if data.article.author}
-			<div>By <span>{data.article.author.replace('@', '')}</span></div>
-		{/if}
-		{#if data.article.published}
-			<div>Published on <span>{new Date(data.article.published).toLocaleDateString()}</span></div>
-		{/if}
-	</div>
-	{@html data.article.content}
 </article>
 
 <style>
