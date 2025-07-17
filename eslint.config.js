091769c0210a21ffc371b81072fdcc8a632ec41c
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2025-07-17 22:21:10 +0300

    Updated deps and eslint config. Specify node 22 for building

--- eslint.config.js
+++ eslint.config.js
@@ -3,14 +3,18 @@ import ts from 'typescript-eslint';
 import svelte from 'eslint-plugin-svelte';
 import prettier from 'eslint-config-prettier';
 import globals from 'globals';
+import svelteConfig from './svelte.config.js';
+import { fileURLToPath } from 'node:url';
+import { includeIgnoreFile } from '@eslint/compat';
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
+export default ts.config(
+	includeIgnoreFile(gitignorePath),
 	js.configs.recommended,
 	...ts.configs.recommended,
-	...svelte.configs['flat/recommended'],
+	...svelte.configs.recommended,
 	prettier,
-	...svelte.configs['flat/prettier'],
+	...svelte.configs.prettier,
 	{
 		languageOptions: {
 			globals: {
@@ -20,19 +24,35 @@ export default [
 		},
 	},
 	{
-		files: ['**/*.svelte'],
+		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+		ignores: ['eslint.config.js', 'svelte.config.js'],
+
 		languageOptions: {
 			parserOptions: {
+				projectService: true,
+				extraFileExtensions: ['.svelte'],
 				parser: ts.parser,
+				svelteConfig,
 			},
 		},
 	},
 	{
-		ignores: ['build/', '.svelte-kit/', 'package/'],
-	},
-	{
 		rules: {
+			'no-constant-condition': 0,
 			'no-self-assign': 'off',
+			'@typescript-eslint/no-explicit-any': 0,
+			'@typescript-eslint/ban-ts-comment': 0,
+			'@typescript-eslint/ban-types': 0,
+			'@typescript-eslint/no-non-null-assertion': 0,
+			'@typescript-eslint/no-unused-vars': [
+				'warn',
+				{
+					args: 'after-used',
+					argsIgnorePattern: '^_',
+					destructuredArrayIgnorePattern: '^_',
+					ignoreRestSiblings: true,
+				},
+			],
 		},
 	},
-];
+);
